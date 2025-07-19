b6e03cbafe178bb30a10bc31f2be6aa50726dd52
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2025-07-19 18:44:35 +0200

    Integrate Supabase for authentication and add necessary plugins for app functionality

---
+++ lib/core/secrets/app_secrets.dart
@@ -0,0 +1,5 @@
+class AppSecrets {
+  static const String supabaseUrl = 'https://cjwviotgqowwbvdbphvz.supabase.co';
+  static const String supabaseAnonKey =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNqd3Zpb3RncW93d2J2ZGJwaHZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTI5NDIwODEsImV4cCI6MjA2ODUxODA4MX0.fCp2tuWvORAPABJ64e01eHkjow0HalQ2brFz0FOHto4';
+}
