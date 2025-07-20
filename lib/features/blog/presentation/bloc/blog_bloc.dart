959bbad1231f5ad0bdc5d84d57f63ecb7b665e02
Bon Tertius Tuyishimire <tuyishimirebt12@gmail.com>
2025-07-20 19:21:49 +0200

    Implement BlogBloc, BlogEvent, and BlogState for blog upload functionality

---
+++ lib/features/blog/presentation/bloc/blog_bloc.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:blog_app/features/blog/domain/usecases/upload_blog.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'blog_event.dart';
+part 'blog_state.dart';
+
+class BlogBloc extends Bloc<BlogEvent, BlogState> {
+  final UploadBlog uploadBlog;
+
+  BlogBloc(this.uploadBlog) : super(BlogInitial()) {
+    on<BlogEvent>((_, emit) => emit(BlogLoading()));
+    on<BlogUpload>(_onBlogUpload);
+  }
+
+  void _onBlogUpload(BlogUpload event, Emitter<BlogState> emit) async {
+    final res = await uploadBlog(
+      UploadBlogParams(
+        posterId: event.posterId,
+        title: event.title,
+        content: event.content,
+        image: event.image,
+        topics: event.topics,
+      ),
+    );
+    res.fold((l) => BlogFailure(l.message), (r) => BlogSuccess());
+  }
+}
